8b32f49cbc282f636a7c6ad9819a46a4e80c1e79
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2023-09-25 22:21:48 +0900

    cctv 6번생성후 cctv위치조정
    5번이랑 6번 생겼는데 5번위 처음 나누는 노드 위쪽 6번이 아래쪽

--- Assets/Scripts/Legacy/CctvController.cs
+++ Assets/Scripts/Legacy/CctvController.cs
@@ -6,16 +6,18 @@ public class CctvController : MonoBehaviour
 {
     int a_width;
     int a_height;
-     [SerializeField] public Vector3 cctv_1_po;
+     public Vector3 cctv_1_po;
      public Vector3 cctv_2_po;
      public Vector3 cctv_3_po;
      public Vector3 cctv_4_po;
      public  Vector3 cctv_5_po;
+     public  Vector3 cctv_6_po;
      public Vector3 cctv_1_ro;
     public  Vector3 cctv_2_ro;
      public Vector3 cctv_3_ro;
      public Vector3 cctv_4_ro;
      public  Vector3 cctv_5_ro;
+     public  Vector3 cctv_6_ro;
 public void Start()
     {
         // A 스크립트를 가진 게임 오브젝트를 찾습니다.
@@ -30,21 +32,23 @@ public void Start()
             Debug.Log("ahffk" + bRect);
             Debug.Log("받아온 width : " + a_width + "받아온 height" + a_height);
             // bRect를 사용할 수 있습니다.
+        
         }
-        abc();
+    abc();
     }
-
     public void abc(){
-    cctv_1_po = new Vector3(-0.8f, 3.8f, -0.9f);
-    cctv_2_po = new Vector3(-0.8f, 3.8f, 50.5f);
-    cctv_3_po = new Vector3(50.5f, 3.8f, 50.5f);
-    cctv_4_po = new Vector3(50.5f, 3.8f, -0.8f);
-    cctv_5_po = new Vector3(a_width, 3.8f, a_height);
+    cctv_1_po = new Vector3(-0.8f, 2.7f, -0.8f);
+    cctv_2_po = new Vector3(-0.8f, 2.7f, 50.5f);
+    cctv_3_po = new Vector3(50.5f, 2.7f, 50.5f);
+    cctv_4_po = new Vector3(50.5f, 2.7f, -0.8f);
+    cctv_5_po = new Vector3(a_width, 2, a_height-2);
+    cctv_6_po = new Vector3(a_width, 2, a_height+2);
     cctv_1_ro = new Vector3(31, 44, 0);
-    cctv_2_ro = new Vector3(31, 130, 0);
-    cctv_3_ro = new Vector3(31, -140, 0);
-    cctv_4_ro = new Vector3(31, -40, 0);
+    cctv_2_ro = new Vector3(31, 135, 0);
+    cctv_3_ro = new Vector3(31, -135, 0);
+    cctv_4_ro = new Vector3(31, -45, 0);
     cctv_5_ro = new Vector3(31, 0, 0);
+    cctv_6_ro = new Vector3(31, 180, 0);
     }
     public void MoveCCTV1()
     {
@@ -70,5 +74,10 @@ public void Start()
     {
         transform.position = cctv_5_po;
         transform.rotation = Quaternion.Euler(cctv_5_ro);
+    }
+    public void MoveCCTV6()
+    {
+        transform.position = cctv_6_po;
+        transform.rotation = Quaternion.Euler(cctv_6_ro);
     }
 }
